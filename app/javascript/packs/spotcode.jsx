14e227f9ff9403e988e5fafcb2af8554b9ae5543
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-04-15 17:59:14 -0300

    Criando a Página Home

--- app/javascript/packs/spotcode.jsx
+++ app/javascript/packs/spotcode.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from '../app';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   ReactDOM.render(
     <App/>,
     document.getElementById('root'),
