9947ad0d3a701c9c4a1a95108212bea4aa238dae
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-04-17 14:58:48 -0300

    Conectando a página discovery com o Backend

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -6,19 +6,19 @@ class Api::V1::DashboardController < ApplicationController
 
   private
 
-
-  def load_recent_heard
-    @recent_albums = current_user.recently_heards.order("created_at DESC").limit(4).map(&:album)
-  end
-
-
-  def load_recommendations
-    heard_categories = @recent_albums.map(&:category)
-    if heard_categories.present?
-      @recommend_albums = Album.joins(:category, :songs).where(category: heard_categories).order("songs.played_count")
-                                .select("distinct albums.*").limit(12)
-    else
-      @recommend_albums = Album.all.limit(12)
+    def load_recent_heard
+      @recent_albums = current_user.recently_heards.order('created_at DESC').limit(4).map(&:album)
     end
-  end
+
+    def load_recommendations
+      heard_categories = @recent_albums.map(&:category)
+      if heard_categories.present?
+        @recommended_albums = Album.joins(:category, :songs).where(category: heard_categories)
+                                                            .order('songs.played_count')
+                                                            .select('distinct albums.*')
+                                                            .limit(12)
+      else
+        @recommended_albums = Album.all.sample(12)
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,8 @@ Rails.application.routes.draw do
 
       resources :songs, only: [] do
         concerns :favoritable, favoritable_type: 'Song'
-      end 
+      end
     end
-  end  
+  end
   get "*path", to: "home#index", :constraints => lambda{|req| req.path !~ /\.(png|jpg|js|css|json)$/ }
 end
