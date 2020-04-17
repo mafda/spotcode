799ee88a349b7cb55956a59182225d557f7404da
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-04-17 19:00:02 -0300

    Player de música (exibindo as músicas reais)

--- app/javascript/components/musics/music/index.js
+++ app/javascript/components/musics/music/index.js
@@ -13,30 +13,38 @@ const CustomSubHeading = styled(Heading)`
 `
 
 const Music = (props) => {
+
+  let PlayingButton;
+  if(props.playing) {
+    PlayingButton = <FaStopCircle size='45px' className='has-text-white' onClick={() => props.setPlaying([])}/>
+  } else {
+    PlayingButton = <FaPlayCircle size='45px' className='has-text-white' onClick={() => props.setPlaying(props.song)}/>
+  }
+
   return (
     <Fragment>
-     <Columns className='is-vcentered is-mobile is-centered'>
-       <Columns.Column desktop={{ size: 1 }} mobile={{ size: 2 }} >
-         <FaPlayCircle size='45px' className='has-text-white' />
-       </Columns.Column>
-       <Columns.Column desktop={{ size: 4 }} mobile={{ size: 8 }}>
-         <Columns className='is-vcentered is-mobile'>
-           <Columns.Column desktop={{ size: 8 }} mobile={{ size: 8 }}>
-             <Heading size={5} className='has-text-white'>
-              Title
-               </Heading>
-             <CustomSubHeading size={6} className='has-text-white' subtitle>
-              Artist Name
-               </CustomSubHeading>
+      <Columns className='is-vcentered is-mobile is-centered'>
+        <Columns.Column desktop={{ size: 1 }} mobile={{ size: 2 }} >
+          {PlayingButton}
+        </Columns.Column>
+      <Columns.Column desktop={{ size: 4 }} mobile={{ size: 8 }}>
+        <Columns className='is-vcentered is-mobile'>
+          <Columns.Column desktop={{ size: 8 }} mobile={{ size: 8 }}>
+            <Heading size={5} className='has-text-white'>
+              {props.song.title}
+            </Heading>
+            <CustomSubHeading size={6} className='has-text-white' subtitle>
+              {props.song.artist_name}
+            </CustomSubHeading>
            </Columns.Column>
-           <Columns.Column desktop={{ size: 4 }} mobile={{ size: 4 }} className='is-pulled-right has-text-right'>
+          <Columns.Column desktop={{ size: 4 }} mobile={{ size: 4 }} className='is-pulled-right has-text-right'>
             {/* Favorite */}
-           </Columns.Column>
+          </Columns.Column>
          </Columns>
-         <MusicSeparator />
-       </Columns.Column>
-     </Columns>
-   </Fragment>
+        <MusicSeparator />
+      </Columns.Column>
+    </Columns>
+  </Fragment>
   );
 }
 export default Music;
