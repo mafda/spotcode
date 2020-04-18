0c19ca3c187ddfdde7ecdd325f84f2996516b56c
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-04-18 18:34:26 -0300

    Criando a busca por texto e categoria

--- app/javascript/services/categories.js
+++ app/javascript/services/categories.js
@@ -1,7 +1,7 @@
 import Api from './api';
-​
+
 const CategoriesService = {
   show: (id) => Api.get(`/categories/${id}`)
 }
-​
+
 export default CategoriesService;
--- app/javascript/services/search.js
+++ app/javascript/services/search.js
@@ -3,5 +3,5 @@ import Api from './api';
 const SearchService = {
   index: (query) => Api.get(`/search?query=${query}`)
 }
-​
+
 export default SearchService;
